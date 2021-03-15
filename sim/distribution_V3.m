b541e5e8bfd9b0dccd2407561614dec04eb5ee16
andreaegg <eandrea@student.ethz.ch>
2021-03-15 12:45:35 +0100

    distribution_V3: added the option to calculate the sum of several gaussians

--- sim/distribution_V3.m
+++ sim/distribution_V3.m
@@ -1,12 +1,32 @@
-function dist = distribution_V3(V,V0,stdV)
+function pdf = distribution_V3(V,V0,stdV,factor)
 
 if nargin < 2
     V0 = mean(V);
     stdV = std(V)/2;
+    factor = 1;
 end
 
-P = (1/(stdV*sqrt(2*pi)))*exp(-(V-V0).^2/(2*stdV^2));
-dist.x   = V;
-dist.pdf = P/sum(P);
+if nargin < 3
+    stdV = std(V)/2;
+    factor = 1;
+end
+
+if nargin < 4
+    factor = 1;
+end
+
+P = zeros(size(V));
+
+if size(V0) == size(stdV)
+    if size(V0) == size(factor)
+        if size(stdV) == size(factor)
+            for k = 1:length(V0)
+                P = P + (factor(k)/(stdV(k)*sqrt(2*pi)))*exp(-(V-V0(k)).^2/(2*stdV(k)^2));
+            end
+        end
+    end
+end
+
+pdf = P/sum(P);
 
 end
