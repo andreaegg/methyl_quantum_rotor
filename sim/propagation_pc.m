f8cda16fe323458cc6b5e718774b8638d44c1395
andreaegg <eandrea@student.ethz.ch>
2021-03-22 18:05:55 +0100

    Adjustment that propagators are calculated mathematically

--- sim/propagation_pc.m
+++ sim/propagation_pc.m
@@ -1,20 +1,19 @@
-function [sig_pc] = propagation_pc(sigma,propagator,addphase)
+function [sig_pc] = propagation_pc(sigma,propagator,propagatorr,addphase)
 
 if isempty(addphase)
     addphase = ["+","-"];
 end
 
-sig_pc = zeros(size(propagator{1}));
-
-if length(propagator) == 1
-    sig_pc = propagator{1}*sigma*propagator{1}';
+if ~iscell(propagator)
+    sig_pc = propagator*sigma*propagatorr;
 else
+    sig_pc = zeros(size(propagator{1}));
     for k = 1:length(propagator)
         switch addphase(k)
             case "-"
-                sig_tmp = -propagator{k}*sigma*propagator{k}';
+                sig_tmp = -propagator{k}*sigma*propagatorr{k};
             case "+"
-                sig_tmp = propagator{k}*sigma*propagator{k}';
+                sig_tmp = propagator{k}*sigma*propagatorr{k};
         end
         sig_pc = sig_pc + sig_tmp/length(propagator);
     end
