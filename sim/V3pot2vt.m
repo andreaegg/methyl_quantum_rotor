275b919cbeb882db1465f74ce3e64cb56dd56254
andreaegg <eandrea@student.ethz.ch>
2021-05-10 10:10:56 +0200

    Corrected minor bugs

--- sim/V3pot2vt.m
+++ sim/V3pot2vt.m
@@ -18,20 +18,7 @@ end
 m = -nreigval:1:nreigval;
 n = -nreigval:1:nreigval;
 
-% Calculate Energy Eigenvalues
-E = zeros(length(m),length(V3));
-for k = 1:length(V3)
-    currpot = V3(k);
-    H = rotorhamiltonian(currpot,m,n,B);
-    En = eig(H);
-    En = sort(En);
-    E(:,k) = En;
-end
-
-% Calculate Tunnel Frequency
-for k = 1:length(V3)
-    nut(k) = E(2,k) - E(1,k);
-end
+[nut,E] = hindered_rotor_model(V3,m,n,B);
 
 vt.kHz = nut*boltzm/planck/1e3;
 vt.MHz = nut*boltzm/planck/1e6;
